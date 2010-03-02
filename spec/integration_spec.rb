f7cd05afcf001aa0d2c80fb005ffaeceb46000e7
grosser <grosser.michael@gmail.com>
2010-03-02 20:48:17 +0100

    fix for slower integration server

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -78,6 +78,6 @@ describe 'CLI' do
     write 'xxx6_spec.rb', 'describe("it"){it("should"){sleep 2}}'
     t = Time.now
     run_specs :processes => 6
-    (Time.now - t).should < 5
+    (Time.now - t).should < (Parallel.processor_count == 1 ? 10 : 5)
   end
 end
