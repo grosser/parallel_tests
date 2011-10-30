596a72fa0097d5326eca0a241fe0b1a491af3550
grosser <grosser.michael@gmail.com>
2011-10-30 15:43:46 -0700

    simplify grouper even more

--- lib/parallel_tests/grouper.rb
+++ lib/parallel_tests/grouper.rb
@@ -1,24 +1,25 @@
 class ParallelTests
   class Grouper
     def self.in_groups(items, num_groups)
-      [].tap do |groups|
-        while ! items.empty?
-          (0...num_groups).map do |group_number|
-            groups[group_number] ||= []
-            groups[group_number] << items.shift
-          end
+      groups = Array.new(num_groups){ [] }
+
+      until items.empty?
+        num_groups.times do |group_number|
+          groups[group_number] << items.shift
         end
-      end.map(&:sort)
+      end
+
+      groups.map!(&:sort!)
     end
 
     def self.in_even_groups_by_size(items_with_sizes, num_groups)
-      items_with_size = smallest_first(items_with_sizes)
       groups = Array.new(num_groups){{:items => [], :size => 0}}
-      items_with_size.each do |item, size|
+
+      smallest_first(items_with_sizes).each do |item, size|
         add_to_smallest_group(groups, item, size)
       end
 
-      groups.map{|g| g[:items].sort }
+      groups.map!{|g| g[:items].sort }
     end
 
     def self.smallest_first(files)
