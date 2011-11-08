8c40dcbda4335cb8b96e43e072e058c40bc8f396
grosser <grosser.michael@gmail.com>
2011-11-07 19:39:36 -0800

    more specs for runtime logger

--- spec/parallel_specs/spec_runtime_logger_spec.rb
+++ spec/parallel_specs/spec_runtime_logger_spec.rb
@@ -10,15 +10,48 @@ describe ParallelSpecs::SpecRuntimeLogger do
     ENV.delete 'TEST_ENV_NUMBER'
   end
 
-  it "logs runtime with relative paths" do
-    Tempfile.open('xxx') do |f|
-      logger = ParallelSpecs::SpecRuntimeLogger.new(f)
+  def log_for_a_file
+    Tempfile.open('xxx') do |temp|
+      temp.close
+      f = File.open(temp.path,'w')
+      logger = if block_given?
+        yield(f)
+      else
+        ParallelSpecs::SpecRuntimeLogger.new(f)
+      end
       logger.example_started
       logger.example_passed(mock(:location => "#{Dir.pwd}/spec/foo.rb:123"))
       logger.start_dump
 
-      f.close
-      File.read(f.path).should =~ %r{^spec/foo.rb:0.\d+$}m
+      #f.close
+      return File.read(f.path)
     end
+  end
+
+  it "logs runtime with relative paths" do
+    log_for_a_file.should =~ %r{^spec/foo.rb:0.\d+$}m
+  end
+
+  it "does not log if we do not run in parallel" do
+    ENV.delete 'TEST_ENV_NUMBER'
+    log_for_a_file.should == ''
+  end
+
+  it "appends to a given file" do
+    result = log_for_a_file do |f|
+      f.write 'FooBar'
+      ParallelSpecs::SpecRuntimeLogger.new(f)
+    end
+    result.should include('FooBar')
+    result.should include('foo.rb')
+  end
+
+  it "overwrites a given path" do
+    result = log_for_a_file do |f|
+      f.write 'FooBar'
+      ParallelSpecs::SpecRuntimeLogger.new(f.path)
+    end
+    result.should_not include('FooBar')
+    result.should include('foo.rb')
   end
 end
