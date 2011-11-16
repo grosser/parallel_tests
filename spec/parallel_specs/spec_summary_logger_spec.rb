539218cfd44f4c4afd5e8d2dc669d4ba88056ec0
grosser <grosser.michael@gmail.com>
2011-11-15 21:56:34 -0800

    remove calls to old methods

--- spec/parallel_specs/spec_summary_logger_spec.rb
+++ spec/parallel_specs/spec_summary_logger_spec.rb
@@ -4,12 +4,11 @@ describe ParallelSpecs::SpecSummaryLogger do
   let(:output){ OutputLogger.new([]) }
   let(:logger){ ParallelSpecs::SpecSummaryLogger.new(output) }
 
-  it "should print a summary of failing examples" do
+  it "prints failing examples" do
     logger.example_failed mock(:location => '/my/spec/path/to/example.rb:123', :description => 'should do stuff', :header => 'HEAD', :exception => mock(:backtrace => []))
     logger.example_failed mock(:location => '/my/spec/path/to/example.rb:125', :description => 'should not do stuff', :header => 'HEAD', :exception => mock(:backtrace => []))
-    logger.dump_failure
-    output.output[0].should == '2 examples failed:'
-    output.output[-2..-1].should == [
+    logger.dump_failures
+    output.output.should == [
       "bundle exec rspec ./spec/path/to/example.rb:123 # should do stuff",
       "bundle exec rspec ./spec/path/to/example.rb:125 # should not do stuff"
     ]
@@ -17,7 +16,7 @@ describe ParallelSpecs::SpecSummaryLogger do
 
   it "does not print anything for passing examples" do
     logger.example_passed mock(:location => "/my/spec/foo.rb:123")
-    logger.dump_failure
+    logger.dump_failures
     output.output.should == []
     logger.dump_summary(1,2,3,4)
     output.output.should == ["2 run, 3 failed, 4 pending"]
@@ -25,7 +24,7 @@ describe ParallelSpecs::SpecSummaryLogger do
 
   it "does not print anything for pending examples" do
     logger.example_pending mock(:location => "/my/spec/foo.rb:123")
-    logger.dump_failure
+    logger.dump_failures
     output.output.should == []
     logger.dump_summary(1,2,3,4)
     output.output.should == ["2 run, 3 failed, 4 pending"]
