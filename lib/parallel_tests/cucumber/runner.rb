52f236f1605620f2601dd51f9d37144cc3a9bd42
Pedro Carriço <pedro.carrico@seedrs.com>
2012-04-02 11:01:15 +0100

    Add support for loading a file with additional options for cucumber

--- lib/parallel_tests/cucumber/runner.rb
+++ lib/parallel_tests/cucumber/runner.rb
@@ -6,6 +6,7 @@ module ParallelTests
       def self.run_tests(test_files, process_number, options)
         color = ($stdout.tty? ? 'AUTOTEST=1 ; export AUTOTEST ;' : '')#display color when we are in a terminal
         runtime_logging = " --format ParallelTests::Cucumber::RuntimeLogger --out #{runtime_log}"
+        options[:test_options] = options[:test_options].nil? ? cucumber_opts : "#{options[:test_options]} #{cucumber_opts}".strip
         cmd = "#{color} #{executable}"
         cmd << runtime_logging if File.directory?(File.dirname(runtime_log))
         cmd << " #{options[:test_options]} #{test_files*' '}"
@@ -36,6 +37,14 @@ module ParallelTests
 
       def self.line_is_result?(line)
         line =~ /^\d+ (steps|scenarios)/
+      end
+
+      def self.cucumber_opts
+        if File.exists?('.cucumber_parallel')
+          File.read('.cucumber_parallel').strip
+        else
+          ''
+        end
       end
     end
   end
