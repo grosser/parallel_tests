09538aea66365d20f16f8ec3e886c2d6e3a62996
Zeke Fast <zekefast@gmail.com>
2012-10-17 16:16:18 +0300

    Replace double quotes with single ones to be consecutive.

--- spec/parallel_tests/tasks_spec.rb
+++ spec/parallel_tests/tasks_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 require 'parallel_tests/tasks'
 
 describe ParallelTests::Tasks do
-  describe ".parse_args" do
-    it "should return the count" do
+  describe '.parse_args' do
+    it 'should return the count' do
       args = {:count => 2}
       ParallelTests::Tasks.parse_args(args).should == [2, '', '', '']
     end
@@ -39,123 +39,123 @@ describe ParallelTests::Tasks do
     end
   end
 
-  describe ".rails_env" do
+  describe '.rails_env' do
     around do |example|
       begin
-        old = ENV["RAILS_ENV"]
-        ENV.delete "RAILS_ENV"
+        old = ENV['RAILS_ENV']
+        ENV.delete 'RAILS_ENV'
         example.call
       ensure
-        ENV["RAILS_ENV"] = old
+        ENV['RAILS_ENV'] = old
       end
     end
 
-    it "should be test when nothing was set" do
-      ParallelTests::Tasks.rails_env.should == "test"
+    it 'should be test when nothing was set' do
+      ParallelTests::Tasks.rails_env.should == 'test'
     end
 
-    it "should be whatever was set" do
-      ENV["RAILS_ENV"] = "foo"
-      ParallelTests::Tasks.rails_env.should == "foo"
+    it 'should be whatever was set' do
+      ENV['RAILS_ENV'] = 'foo'
+      ParallelTests::Tasks.rails_env.should == 'foo'
     end
   end
 
-  describe ".run_in_parallel" do
-    let(:full_path){ File.expand_path("../../../bin/parallel_test", __FILE__) }
+  describe '.run_in_parallel' do
+    let(:full_path){ File.expand_path('../../../bin/parallel_test', __FILE__) }
 
-    it "should have the executable" do
+    it 'should have the executable' do
       File.file?(full_path).should == true
       File.executable?(full_path).should == true
     end
 
-    it "runs command in parallel" do
+    it 'runs command in parallel' do
       ParallelTests::Tasks.should_receive(:system).with("#{full_path} --exec 'echo'").and_return true
-      ParallelTests::Tasks.run_in_parallel("echo")
+      ParallelTests::Tasks.run_in_parallel('echo')
     end
 
-    it "runs command with :count option" do
+    it 'runs command with :count option' do
       ParallelTests::Tasks.should_receive(:system).with("#{full_path} --exec 'echo' -n 123").and_return true
-      ParallelTests::Tasks.run_in_parallel("echo", :count => 123)
+      ParallelTests::Tasks.run_in_parallel('echo', :count => 123)
     end
 
-    it "runs command with :non_parallel option" do
+    it 'runs command with :non_parallel option' do
       ParallelTests::Tasks.should_receive(:system).with("#{full_path} --exec 'echo' --non-parallel").and_return true
-      ParallelTests::Tasks.run_in_parallel("echo", :non_parallel => true)
+      ParallelTests::Tasks.run_in_parallel('echo', :non_parallel => true)
     end
 
-    it "runs aborts if the command fails" do
+    it 'runs aborts if the command fails' do
       ParallelTests::Tasks.should_receive(:system).and_return false
       ParallelTests::Tasks.should_receive(:abort).and_return false
-      ParallelTests::Tasks.run_in_parallel("echo")
+      ParallelTests::Tasks.run_in_parallel('echo')
     end
   end
 
-  describe ".suppress_output" do
+  describe '.suppress_output' do
     def call(command, grep)
       result = `#{ParallelTests::Tasks.suppress_output(command, grep)}`
       [result, $?.success?]
     end
 
-    context "with pipefail supported" do
+    context 'with pipefail supported' do
       before :all do
-        if not system("set -o pipefail && test 1")
-          pending "pipefail is not supported on your system"
+        if not system('set -o pipefail && test 1')
+          pending 'pipefail is not supported on your system'
         end
       end
 
-      it "should hide offending lines" do
-        call("echo 123", "123").should == ["", true]
+      it 'should hide offending lines' do
+        call('echo 123', '123').should == ['', true]
       end
 
-      it "should not hide other lines" do
-        call("echo 124", "123").should == ["124\n", true]
+      it 'should not hide other lines' do
+        call('echo 124', '123').should == ["124\n", true]
       end
 
-      it "should fail if command fails and the pattern matches" do
-        call("echo 123 && test", "123").should == ["", false]
+      it 'should fail if command fails and the pattern matches' do
+        call('echo 123 && test', '123').should == ['', false]
       end
 
-      it "should fail if command fails and the pattern fails" do
-        call("echo 124 && test", "123").should == ["124\n", false]
+      it 'should fail if command fails and the pattern fails' do
+        call('echo 124 && test', '123').should == ["124\n", false]
       end
     end
 
-    context "without pipefail supported" do
+    context 'without pipefail supported' do
       before do
-        ParallelTests::Tasks.should_receive(:system).with("set -o pipefail && test 1").and_return false
+        ParallelTests::Tasks.should_receive(:system).with('set -o pipefail && test 1').and_return false
       end
 
-      it "should not filter and succeed" do
-        call("echo 123", "123").should == ["123\n", true]
+      it 'should not filter and succeed' do
+        call('echo 123', '123').should == ["123\n", true]
       end
 
-      it "should not filter and fail" do
-        call("echo 123 && test", "123").should == ["123\n", false]
+      it 'should not filter and fail' do
+        call('echo 123 && test', '123').should == ["123\n", false]
       end
     end
   end
 
-  describe ".check_for_pending_migrations" do
+  describe '.check_for_pending_migrations' do
     after do
-      Rake.application.instance_variable_get('@tasks').delete("db:abort_if_pending_migrations")
+      Rake.application.instance_variable_get('@tasks').delete('db:abort_if_pending_migrations')
     end
 
-    it "should do nothing if pending migrations is no defined" do
+    it 'should do nothing if pending migrations is no defined' do
       ParallelTests::Tasks.check_for_pending_migrations
     end
 
-    it "should run pending migrations is task is defined" do
+    it 'should run pending migrations is task is defined' do
       foo = 1
-      Rake::Task.define_task("db:abort_if_pending_migrations") do
+      Rake::Task.define_task('db:abort_if_pending_migrations') do
         foo = 2
       end
       ParallelTests::Tasks.check_for_pending_migrations
       foo.should == 2
     end
 
-    it "should not execute the task twice" do
+    it 'should not execute the task twice' do
       foo = 1
-      Rake::Task.define_task("db:abort_if_pending_migrations") do
+      Rake::Task.define_task('db:abort_if_pending_migrations') do
         foo += 1
       end
       ParallelTests::Tasks.check_for_pending_migrations
--- spec/parallel_tests/test/runner_spec.rb
+++ spec/parallel_tests/test/runner_spec.rb
@@ -8,22 +8,22 @@ describe ParallelTests::Test::Runner do
       ParallelTests::Test::Runner.run_tests(*args)
     end
 
-    it "uses TEST_ENV_NUMBER=blank when called for process 0" do
+    it 'uses TEST_ENV_NUMBER=blank when called for process 0' do
       ParallelTests::Test::Runner.should_receive(:open).with{|x,y|x=~/TEST_ENV_NUMBER= /}.and_return mocked_process
       call(['xxx'],0,{})
     end
 
-    it "uses TEST_ENV_NUMBER=2 when called for process 1" do
+    it 'uses TEST_ENV_NUMBER=2 when called for process 1' do
       ParallelTests::Test::Runner.should_receive(:open).with{|x,y| x=~/TEST_ENV_NUMBER=2/}.and_return mocked_process
       call(['xxx'],1,{})
     end
 
-    it "uses options" do
+    it 'uses options' do
       ParallelTests::Test::Runner.should_receive(:open).with{|x,y| x=~ %r{ruby -Itest .* -- -v}}.and_return mocked_process
       call(['xxx'],1,:test_options => '-v')
     end
 
-    it "returns the output" do
+    it 'returns the output' do
       io = open('spec/spec_helper.rb')
       $stdout.stub!(:print)
       ParallelTests::Test::Runner.should_receive(:open).and_return io
@@ -36,28 +36,28 @@ describe ParallelTests::Test::Runner do
       ParallelTests::Test::Runner.tests_in_groups(*args)
     end
 
-    it "does not sort when passed false do_sort option" do
+    it 'does not sort when passed false do_sort option' do
       ParallelTests::Test::Runner.should_not_receive(:smallest_first)
       call [], 1, :group_by => :found
     end
 
-    it "does sort when not passed do_sort option" do
+    it 'does sort when not passed do_sort option' do
       ParallelTests::Test::Runner.stub!(:tests_with_runtime).and_return([])
       ParallelTests::Grouper.should_receive(:largest_first).and_return([])
       call [], 1
     end
 
-    it "groups by single_process pattern and then via size" do
+    it 'groups by single_process pattern and then via size' do
       ParallelTests::Test::Runner.should_receive(:with_runtime_info).and_return([['aaa',5],['aaa2',5],['bbb',2],['ccc',1],['ddd',1]])
       result = call [], 3, :single_process => [/^a.a/]
-      result.should == [["aaa", "aaa2"], ["bbb"], ["ccc", "ddd"]]
+      result.should == [['aaa', 'aaa2'], ['bbb'], ['ccc', 'ddd']]
     end
 
-    it "groups by size and adds isolated separately" do
+    it 'groups by size and adds isolated separately' do
       ParallelTests::Grouper.should_receive(:isolate!).with([], /^aaa/).and_return(['aaa'])
       ParallelTests::Test::Runner.should_receive(:with_runtime_info).and_return([['bbb',3],['ccc',1],['ddd',2]])
       result = call [], 3, :isolate => /^aaa/
-      result.should == [["bbb"], ["ccc", "ddd"], ["aaa"]]
+      result.should == [['bbb'], ['ccc', 'ddd'], ['aaa']]
     end
   end
 
@@ -66,7 +66,7 @@ describe ParallelTests::Test::Runner do
       ParallelTests::Test::Runner.find_results(*args)
     end
 
-    it "finds multiple results in test output" do
+    it 'finds multiple results in test output' do
       output = <<EOF
 Loaded suite /opt/ruby-enterprise/lib/ruby/gems/1.8/gems/rake-0.8.4/lib/rake/rake_test_loader
 Started
@@ -86,7 +86,7 @@ EOF
       call(output).should == ['10 tests, 20 assertions, 0 failures, 0 errors','14 tests, 20 assertions, 0 failures, 0 errors']
     end
 
-    it "is robust against scrambled output" do
+    it 'is robust against scrambled output' do
       output = <<EOF
 Loaded suite /opt/ruby-enterprise/lib/ruby/gems/1.8/gems/rake-0.8.4/lib/rake/rake_test_loader
 Started
@@ -105,7 +105,7 @@ EOF
       call(output).should == ['10 tests, 20 assertions, 0 failures, 0 errors','14 tedsts, 20 assertions, 0 failures, 0 errors']
     end
 
-    it "ignores color-codes" do
+    it 'ignores color-codes' do
       output = <<EOF
 10 tests, 20 assertions, 0 \e[31mfailures, 0 errors
 EOF
@@ -141,7 +141,7 @@ EOF
       Dir.chdir old
     end
 
-    it "finds test in folders with appended /" do
+    it 'finds test in folders with appended /' do
       with_files(['b/a_test.rb']) do |root|
         call(["#{root}/"]).sort.should == [
           "#{root}/b/a_test.rb",
@@ -149,7 +149,7 @@ EOF
       end
     end
 
-    it "finds test files nested in symlinked folders" do
+    it 'finds test files nested in symlinked folders' do
       with_files(['a/a_test.rb','b/b_test.rb']) do |root|
         `ln -s #{root}/a #{root}/b/link`
         call(["#{root}/b"]).sort.should == [
@@ -159,7 +159,7 @@ EOF
       end
     end
 
-    it "finds test files but ignores those in symlinked folders" do
+    it 'finds test files but ignores those in symlinked folders' do
       with_files(['a/a_test.rb','b/b_test.rb']) do |root|
         `ln -s #{root}/a #{root}/b/link`
         call(["#{root}/b"], :symlinks => false).sort.should == [
@@ -168,7 +168,7 @@ EOF
       end
     end
 
-    it "finds test files nested in different folders" do
+    it 'finds test files nested in different folders' do
       with_files(['a/a_test.rb','b/b_test.rb', 'c/c_test.rb']) do |root|
         call(["#{root}/a", "#{root}/b"]).sort.should == [
           "#{root}/a/a_test.rb",
@@ -177,7 +177,7 @@ EOF
       end
     end
 
-    it "only finds tests in folders" do
+    it 'only finds tests in folders' do
       with_files(['a/a_test.rb', 'a/test.rb', 'a/test_helper.rb']) do |root|
         call(["#{root}/a"]).sort.should == [
           "#{root}/a/a_test.rb"
@@ -185,7 +185,7 @@ EOF
       end
     end
 
-    it "finds tests in nested folders" do
+    it 'finds tests in nested folders' do
       with_files(['a/b/c/d/a_test.rb']) do |root|
         call(["#{root}/a"]).sort.should == [
           "#{root}/a/b/c/d/a_test.rb"
@@ -193,40 +193,40 @@ EOF
       end
     end
 
-    it "does not expand paths" do
+    it 'does not expand paths' do
       with_files(['a/x_test.rb']) do |root|
         inside_dir root do
           call(['a']).sort.should == [
-            "a/x_test.rb"
+            'a/x_test.rb'
           ]
         end
       end
     end
 
-    it "finds test files in folders by pattern" do
+    it 'finds test files in folders by pattern' do
       with_files(['a/x_test.rb','a/y_test.rb','a/z_test.rb']) do |root|
         inside_dir root do
-          call(["a"], :pattern => /^a\/(y|z)_test/).sort.should == [
-            "a/y_test.rb",
-            "a/z_test.rb",
+          call(['a'], :pattern => /^a\/(y|z)_test/).sort.should == [
+            'a/y_test.rb',
+            'a/z_test.rb',
           ]
         end
       end
     end
 
-    it "finds nothing if I pass nothing" do
+    it 'finds nothing if I pass nothing' do
       call(nil).should == []
     end
 
-    it "finds nothing if I pass nothing (empty array)" do
+    it 'finds nothing if I pass nothing (empty array)' do
       call([]).should == []
     end
 
-    it "keeps invalid files" do
+    it 'keeps invalid files' do
       call(['baz']).should == ['baz']
     end
 
-    it "discards duplicates" do
+    it 'discards duplicates' do
       call(['baz','baz']).should == ['baz']
     end
   end
@@ -236,23 +236,23 @@ EOF
       ParallelTests::Test::Runner.summarize_results(*args)
     end
 
-    it "adds results" do
+    it 'adds results' do
       call(['1 foo 3 bar','2 foo 5 bar']).should == '8 bars, 3 foos'
     end
 
-    it "adds results with braces" do
+    it 'adds results with braces' do
       call(['1 foo(s) 3 bar(s)','2 foo 5 bar']).should == '8 bars, 3 foos'
     end
 
-    it "adds same results with plurals" do
+    it 'adds same results with plurals' do
       call(['1 foo 3 bar','2 foos 5 bar']).should == '8 bars, 3 foos'
     end
 
-    it "adds non-similar results" do
+    it 'adds non-similar results' do
       call(['1 xxx 2 yyy','1 xxx 2 zzz']).should == '2 xxxs, 2 yyys, 2 zzzs'
     end
 
-    it "does not pluralize 1" do
+    it 'does not pluralize 1' do
       call(['1 xxx 2 yyy']).should == '1 xxx, 2 yyys'
     end
   end
