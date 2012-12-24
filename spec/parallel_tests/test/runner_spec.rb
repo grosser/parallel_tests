02ee062b4d70b023f7e260a1eace9bcdc49ee5db
Zeke Fast <zekefast@gmail.com>
2012-12-25 01:24:43 +0200

    Revert "Replace double quotes with single ones to be consecutive."
    This reverts commit 09538aea66365d20f16f8ec3e886c2d6e3a62996.
    
    Conflicts:
    	spec/parallel_tests/test/runner_spec.rb

--- spec/parallel_tests/test/runner_spec.rb
+++ spec/parallel_tests/test/runner_spec.rb
@@ -8,22 +8,22 @@ describe ParallelTests::Test::Runner do
       ParallelTests::Test::Runner.run_tests(*args)
     end
 
-    it 'uses TEST_ENV_NUMBER=blank when called for process 0' do
+    it "uses TEST_ENV_NUMBER=blank when called for process 0" do
       ParallelTests::Test::Runner.should_receive(:open).with{|x,y|x=~/TEST_ENV_NUMBER= /}.and_return mocked_process
       call(['xxx'],0,{})
     end
 
-    it 'uses TEST_ENV_NUMBER=2 when called for process 1' do
+    it "uses TEST_ENV_NUMBER=2 when called for process 1" do
       ParallelTests::Test::Runner.should_receive(:open).with{|x,y| x=~/TEST_ENV_NUMBER=2/}.and_return mocked_process
       call(['xxx'],1,{})
     end
 
-    it 'uses options' do
+    it "uses options" do
       ParallelTests::Test::Runner.should_receive(:open).with{|x,y| x=~ %r{ruby -Itest .* -- -v}}.and_return mocked_process
       call(['xxx'],1,:test_options => '-v')
     end
 
-    it 'returns the output' do
+    it "returns the output" do
       io = open('spec/spec_helper.rb')
       $stdout.stub!(:print)
       ParallelTests::Test::Runner.should_receive(:open).and_return io
@@ -36,12 +36,12 @@ describe ParallelTests::Test::Runner do
       ParallelTests::Test::Runner.tests_in_groups(*args)
     end
 
-    it 'does not sort when passed false do_sort option' do
+    it "does not sort when passed false do_sort option" do
       ParallelTests::Test::Runner.should_not_receive(:smallest_first)
       call([], 1, :group_by => :found)
     end
 
-    it 'does sort when not passed do_sort option' do
+    it "does sort when not passed do_sort option" do
       ParallelTests::Test::Runner.stub!(:tests_with_runtime).and_return([])
       ParallelTests::Grouper.should_receive(:largest_first).and_return([])
       call([], 2)
@@ -55,7 +55,7 @@ describe ParallelTests::Test::Runner do
       call([], 1)
     end
 
-    it 'groups by single_process pattern and then via size' do
+    it "groups by single_process pattern and then via size" do
       ParallelTests::Test::Runner.should_receive(:with_runtime_info).
         and_return([
           ['aaa', 5],
@@ -65,10 +65,10 @@ describe ParallelTests::Test::Runner do
           ['ddd', 1]
         ])
       result = call([], 3, :single_process => [/^a.a/])
-      result.should == [['aaa', 'aaa2'], ['bbb'], ['ccc', 'ddd']]
+      result.should == [["aaa", "aaa2"], ["bbb"], ["ccc", "ddd"]]
     end
 
-    it 'groups by size and adds isolated separately' do
+    it "groups by size and adds isolated separately" do
       ParallelTests::Grouper.should_receive(:isolated).with([], [/^aaa/]).
         and_return([[['aaa']], %w[bbb ccc ddd eee]])
       ParallelTests::Test::Runner.should_receive(:with_runtime_info).
@@ -80,7 +80,7 @@ describe ParallelTests::Test::Runner do
         ])
 
       result = call([], 3, :isolate => true, :single_process => [/^aaa/])
-      result.should == [['bbb'], ['ddd'], ['ccc', 'eee'], ['aaa']]
+      result.should == [["bbb"], ["ddd"], ["ccc", "eee"], ["aaa"]]
     end
   end
 
@@ -89,7 +89,7 @@ describe ParallelTests::Test::Runner do
       ParallelTests::Test::Runner.find_results(*args)
     end
 
-    it 'finds multiple results in test output' do
+    it "finds multiple results in test output" do
       output = <<EOF
 Loaded suite /opt/ruby-enterprise/lib/ruby/gems/1.8/gems/rake-0.8.4/lib/rake/rake_test_loader
 Started
@@ -109,7 +109,7 @@ EOF
       call(output).should == ['10 tests, 20 assertions, 0 failures, 0 errors','14 tests, 20 assertions, 0 failures, 0 errors']
     end
 
-    it 'is robust against scrambled output' do
+    it "is robust against scrambled output" do
       output = <<EOF
 Loaded suite /opt/ruby-enterprise/lib/ruby/gems/1.8/gems/rake-0.8.4/lib/rake/rake_test_loader
 Started
@@ -128,7 +128,7 @@ EOF
       call(output).should == ['10 tests, 20 assertions, 0 failures, 0 errors','14 tedsts, 20 assertions, 0 failures, 0 errors']
     end
 
-    it 'ignores color-codes' do
+    it "ignores color-codes" do
       output = <<EOF
 10 tests, 20 assertions, 0 \e[31mfailures, 0 errors
 EOF
@@ -164,7 +164,7 @@ EOF
       Dir.chdir old
     end
 
-    it 'finds test in folders with appended /' do
+    it "finds test in folders with appended /" do
       with_files(['b/a_test.rb']) do |root|
         call(["#{root}/"]).sort.should == [
           "#{root}/b/a_test.rb",
@@ -172,7 +172,7 @@ EOF
       end
     end
 
-    it 'finds test files nested in symlinked folders' do
+    it "finds test files nested in symlinked folders" do
       with_files(['a/a_test.rb','b/b_test.rb']) do |root|
         `ln -s #{root}/a #{root}/b/link`
         call(["#{root}/b"]).sort.should == [
@@ -182,7 +182,7 @@ EOF
       end
     end
 
-    it 'finds test files but ignores those in symlinked folders' do
+    it "finds test files but ignores those in symlinked folders" do
       with_files(['a/a_test.rb','b/b_test.rb']) do |root|
         `ln -s #{root}/a #{root}/b/link`
         call(["#{root}/b"], :symlinks => false).sort.should == [
@@ -191,7 +191,7 @@ EOF
       end
     end
 
-    it 'finds test files nested in different folders' do
+    it "finds test files nested in different folders" do
       with_files(['a/a_test.rb','b/b_test.rb', 'c/c_test.rb']) do |root|
         call(["#{root}/a", "#{root}/b"]).sort.should == [
           "#{root}/a/a_test.rb",
@@ -200,7 +200,7 @@ EOF
       end
     end
 
-    it 'only finds tests in folders' do
+    it "only finds tests in folders" do
       with_files(['a/a_test.rb', 'a/test.rb', 'a/test_helper.rb']) do |root|
         call(["#{root}/a"]).sort.should == [
           "#{root}/a/a_test.rb"
@@ -208,7 +208,7 @@ EOF
       end
     end
 
-    it 'finds tests in nested folders' do
+    it "finds tests in nested folders" do
       with_files(['a/b/c/d/a_test.rb']) do |root|
         call(["#{root}/a"]).sort.should == [
           "#{root}/a/b/c/d/a_test.rb"
@@ -216,40 +216,40 @@ EOF
       end
     end
 
-    it 'does not expand paths' do
+    it "does not expand paths" do
       with_files(['a/x_test.rb']) do |root|
         inside_dir root do
           call(['a']).sort.should == [
-            'a/x_test.rb'
+            "a/x_test.rb"
           ]
         end
       end
     end
 
-    it 'finds test files in folders by pattern' do
+    it "finds test files in folders by pattern" do
       with_files(['a/x_test.rb','a/y_test.rb','a/z_test.rb']) do |root|
         inside_dir root do
-          call(['a'], :pattern => /^a\/(y|z)_test/).sort.should == [
-            'a/y_test.rb',
-            'a/z_test.rb',
+          call(["a"], :pattern => /^a\/(y|z)_test/).sort.should == [
+            "a/y_test.rb",
+            "a/z_test.rb",
           ]
         end
       end
     end
 
-    it 'finds nothing if I pass nothing' do
+    it "finds nothing if I pass nothing" do
       call(nil).should == []
     end
 
-    it 'finds nothing if I pass nothing (empty array)' do
+    it "finds nothing if I pass nothing (empty array)" do
       call([]).should == []
     end
 
-    it 'keeps invalid files' do
+    it "keeps invalid files" do
       call(['baz']).should == ['baz']
     end
 
-    it 'discards duplicates' do
+    it "discards duplicates" do
       call(['baz','baz']).should == ['baz']
     end
   end
@@ -259,23 +259,23 @@ EOF
       ParallelTests::Test::Runner.summarize_results(*args)
     end
 
-    it 'adds results' do
+    it "adds results" do
       call(['1 foo 3 bar','2 foo 5 bar']).should == '8 bars, 3 foos'
     end
 
-    it 'adds results with braces' do
+    it "adds results with braces" do
       call(['1 foo(s) 3 bar(s)','2 foo 5 bar']).should == '8 bars, 3 foos'
     end
 
-    it 'adds same results with plurals' do
+    it "adds same results with plurals" do
       call(['1 foo 3 bar','2 foos 5 bar']).should == '8 bars, 3 foos'
     end
 
-    it 'adds non-similar results' do
+    it "adds non-similar results" do
       call(['1 xxx 2 yyy','1 xxx 2 zzz']).should == '2 xxxs, 2 yyys, 2 zzzs'
     end
 
-    it 'does not pluralize 1' do
+    it "does not pluralize 1" do
       call(['1 xxx 2 yyy']).should == '1 xxx, 2 yyys'
     end
   end
