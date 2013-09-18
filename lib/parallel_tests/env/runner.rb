efb814e85dd86e74d89615e84dac40606444311c
colin harris <qzzzq1@gmail.com>
2013-09-18 13:28:05 -0500

    Spike to create a parallel_test type that optionally pulls test_suffix from env variable.

---
+++ lib/parallel_tests/env/runner.rb
@@ -0,0 +1,27 @@
+require 'open3'
+
+module ParallelTests
+  module Env
+    class Runner < ParallelTests::Test::Runner
+      class << self
+        def name
+          "Env"
+        end
+
+        def runtime_log
+          'tmp/parallel_runtime_env.log'
+        end
+
+        def test_suffix
+          ENV['PARALLEL_TEST_SUFFIX'] || "_test.rb"
+        end
+
+        def run_tests(test_files, process_number, num_processes, options)
+          require_list = test_files.map { |filename| %{"#{File.expand_path filename}"} }.join(" ")
+          cmd = "#{executable} #{require_list}"
+          execute_command(cmd, process_number, num_processes, options)
+        end
+      end
+    end
+  end
+end
