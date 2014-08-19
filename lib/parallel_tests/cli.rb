33691c0307d529fbf92648787a6683e5ce2819b5
Samer Masry <samer@pagerduty.com>
2014-08-19 15:47:36 -0700

    merging groups option with only-group and adding more tests

--- lib/parallel_tests/cli.rb
+++ lib/parallel_tests/cli.rb
@@ -36,11 +36,7 @@ module ParallelTests
         groups = @runner.tests_in_groups(options[:files], num_processes, options)
 
         test_results = if options[:only_group]
-          group_to_run = groups[options[:only_group] - 1]
-
-          [run_tests(group_to_run, 0, num_processes, options)]
-        elsif options[:groups]
-          groups_to_run = options[:groups].collect{|i| groups[i - 1]}
+          groups_to_run = options[:only_group].collect{|i| groups[i - 1]}
           report_number_of_tests(groups_to_run)
           execute_in_parallel(groups_to_run, groups_to_run.size, options) do |group|
             run_tests(group, groups_to_run.index(group), 1, options)
@@ -130,8 +126,7 @@ TEXT
           options[:isolate] = true
         end
 
-        opts.on("--only-group [INTEGER]", Integer) { |group| options[:only_group] = group }
-        opts.on("--groups [INTEGER,INTEGER]", "specify the groups you want to run when the -n option is given", Array){ |groups| options[:groups] = groups.map(&:to_i) }
+        opts.on("--only-group [INTEGER],[INTEGER]", Array) { |groups| options[:only_group] = groups.map(&:to_i) }
 
         opts.on("-e", "--exec [COMMAND]", "execute this code parallel and with ENV['TEST_ENV_NUM']") { |path| options[:execute] = path }
         opts.on("-o", "--test-options '[OPTIONS]'", "execute test commands with those options") { |arg| options[:test_options] = arg }
