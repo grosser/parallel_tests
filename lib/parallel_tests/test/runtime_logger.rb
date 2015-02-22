a915428183f2733610da4fd9c0a7d2d7ce6a2c33
grosser <michael@grosser.it>
2015-02-22 15:55:09 -0800

    simplify and correct runtime logger with source_location

--- lib/parallel_tests/test/runtime_logger.rb
+++ lib/parallel_tests/test/runtime_logger.rb
@@ -4,13 +4,15 @@ require 'parallel_tests/test/runner'
 module ParallelTests
   module Test
     class RuntimeLogger
-      @@previous_log_cleaned = false
+      @@prepared = false
 
       class << self
         # ensure folder exists + clean out previous log
         # this will happen in multiple processes, but should be roughly at the same time
         # so there should be no log message lost
         def prepare
+          return if @@prepared
+          @@prepared = true
           FileUtils.mkdir_p(File.dirname(logfile))
           File.write(logfile, '')
         end
@@ -26,44 +28,10 @@ module ParallelTests
         private
 
         def message(test, start_time, end_time)
-          delta = "%.2f" % (end_time.to_f-start_time.to_f)
-          filename = class_directory(test.class) + class_to_filename(test.class) + ".rb"
+          delta = "%.2f" % (end_time.to_f - start_time.to_f)
+          method = test.public_methods(true).first
+          filename = test.method(method).source_location.first.sub("#{Dir.pwd}/", "")
           "#{filename}:#{delta}"
-        end
-
-        # Note: this is a best guess at conventional test directory structure, and may need
-        # tweaking / post-processing to match correctly for any given project
-        def class_directory(suspect)
-          result = "test/"
-
-          if defined?(Rails)
-            result += case suspect.superclass.name
-            when "ActionDispatch::IntegrationTest"
-              "integration/"
-            when "ActionDispatch::PerformanceTest"
-              "performance/"
-            when "ActionController::TestCase"
-              "functional/"
-            when "ActionView::TestCase"
-              "unit/helpers/"
-            else
-              "unit/"
-            end
-          end
-          result
-        end
-
-        # based on https://github.com/grosser/single_test/blob/master/lib/single_test.rb#L117
-        def class_to_filename(suspect)
-          word = suspect.to_s.dup
-          return word unless word.match /^[A-Z]/ and not word.match %r{/[a-z]}
-
-          word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-          word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-          word.gsub!(/\:\:/, '/')
-          word.tr!("-", "_")
-          word.downcase!
-          word
         end
 
         def locked_appending_to(file)
@@ -87,16 +55,14 @@ end
 
 require 'test/unit/testsuite'
 class ::Test::Unit::TestSuite
-  alias :run_without_timing :run unless defined? @@timing_installed
+  alias_method :run_without_timing, :run
 
-  def run(result, &progress_block)
+  def run(result, &block)
     ParallelTests::Test::RuntimeLogger.prepare
-    first_test = self.tests.first
+    first_test = tests.first
     start_time = ParallelTests.now
-    run_without_timing(result, &progress_block)
+    run_without_timing(result, &block)
     end_time = ParallelTests.now
     ParallelTests::Test::RuntimeLogger.log(first_test, start_time, end_time)
   end
-
-  @@timing_installed = true
 end
--- spec/parallel_tests/test/runtime_logger_spec.rb
+++ spec/parallel_tests/test/runtime_logger_spec.rb
@@ -26,118 +26,29 @@ describe ParallelTests::Test::RuntimeLogger do
       # log looking good ?
       lines = File.read("tmp/parallel_runtime_test.log").split("\n").sort.map { |x|x .sub!(/\d$/, '') }
       lines.should == [
-        "test/foo0.rb:0.5",
-        "test/foo1.rb:0.5",
+        "test/0_test.rb:0.5",
+        "test/1_test.rb:0.5",
       ]
     end
   end
 
-  describe "writing" do
-    around do |example|
-      use_temporary_directory do
-        FileUtils.mkdir_p(File.dirname(log))
-        example.call
+  it 'passes correct test to log' do
+    require 'test/unit'
+    require 'test/unit/ui/xml/testrunner'
+
+    class FakeUnitTest < Test::Unit::TestCase
+      def test_fake
+        assert true
       end
     end
 
-    let(:log) { ParallelTests::Test::Runner.runtime_log }
+    ParallelTests::Test::RuntimeLogger.
+      should_receive(:log).
+      with(kind_of(FakeUnitTest), kind_of(Time),  kind_of(Time))
 
-    it "overwrites the runtime_log file on first log invocation" do
-      class FakeTest
-      end
-      test = FakeTest.new
-      time = Time.now
-      File.open(log, 'w'){ |f| f.puts("FooBar") }
-      ParallelTests::Test::RuntimeLogger.send(:class_variable_set,:@@has_started, false)
-      ParallelTests::Test::RuntimeLogger.log(test, time, Time.at(time.to_f+2.00))
-      result = File.read(log)
-      result.should_not include('FooBar')
-      result.should include('test/fake_test.rb:2.00')
-    end
-
-    it "appends to the runtime_log file after first log invocation" do
-      class FakeTest
-      end
-      test = FakeTest.new
-      class OtherFakeTest
-      end
-      other_test = OtherFakeTest.new
-
-      time = Time.now
-      File.open(log, 'w'){ |f| f.puts("FooBar") }
-      ParallelTests::Test::RuntimeLogger.send(:class_variable_set,:@@has_started, false)
-      ParallelTests::Test::RuntimeLogger.log(test, time, Time.at(time.to_f+2.00))
-      ParallelTests::Test::RuntimeLogger.log(other_test, time, Time.at(time.to_f+2.00))
-      result = File.read(log)
-      result.should_not include('FooBar')
-      result.should include('test/fake_test.rb:2.00')
-      result.should include('test/other_fake_test.rb:2.00')
-    end
-  end
-
-  describe "formatting" do
-    def with_rails_defined
-      Object.const_set(:Rails, Module.new)
-      yield
-      Object.send(:remove_const, :Rails)
-    end
-
-    def call(*args)
-      ParallelTests::Test::RuntimeLogger.send(:message, *args)
-    end
-
-    it "formats results for simple test names" do
-      class FakeTest
-      end
-      test = FakeTest.new
-      time = Time.now
-      call(test, time, Time.at(time.to_f+2.00)).should == 'test/fake_test.rb:2.00'
-    end
-
-    it "formats results for complex test names" do
-      class AVeryComplex
-        class FakeTest
-        end
-      end
-      test = AVeryComplex::FakeTest.new
-      time = Time.now
-      call(test, time, Time.at(time.to_f+2.00)).should == 'test/a_very_complex/fake_test.rb:2.00'
-    end
-
-    it "guesses subdirectory structure for rails test classes" do
-      with_rails_defined do
-        class ActionController
-          class TestCase
-          end
-        end
-        class FakeControllerTest < ActionController::TestCase
-        end
-        test = FakeControllerTest.new
-        time = Time.now
-        call(test, time, Time.at(time.to_f+2.00)).should == 'test/functional/fake_controller_test.rb:2.00'
-      end
-    end
-  end
-
-  describe '::Test::Unit::TestSuite' do
-    it 'passes correct parameters to log' do
-      require 'test/unit'
-      require 'test/unit/ui/xml/testrunner'
-
-      class FakeUnitTest < Test::Unit::TestCase
-        def test_fake
-          assert true
-        end
-      end
-
-      ParallelTests::Test::RuntimeLogger.
-        should_receive(:log).
-        with(kind_of(FakeUnitTest), kind_of(Time),  kind_of(Time))
-
-      my_tests = Test::Unit::TestSuite.new
-      my_tests << FakeUnitTest.new('test_fake')
-      output = StringIO.new
-      Test::Unit::UI::XML::TestRunner.run(my_tests, :output => output)
-    end
+    my_tests = Test::Unit::TestSuite.new
+    my_tests << FakeUnitTest.new('test_fake')
+    output = StringIO.new
+    Test::Unit::UI::XML::TestRunner.run(my_tests, :output => output)
   end
 end
