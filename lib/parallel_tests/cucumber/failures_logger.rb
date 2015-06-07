b2dc7e6b94054a5e5a2f3db95a3084af91332b96
Shashikant <shashikant.jagtap@aol.co.uk>
2015-06-07 11:09:35 +0100

    Added Space to get cucumber logger to work

--- lib/parallel_tests/cucumber/failures_logger.rb
+++ lib/parallel_tests/cucumber/failures_logger.rb
@@ -14,7 +14,7 @@ module ParallelTests
         unless @lines.empty?
           lock_output do
             @lines.each do |line|
-              @io.print "#{feature.file}:#{line}"
+              @io.print "#{feature.file}:#{line} "
             end
           end
         end
