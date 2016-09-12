1c26ba944464c2b254fda208d2d363cba2d6adc1
Urban Hafner <contact@urbanhafner.com>
2016-09-12 13:20:11 +0200

    Test dropping and afterwards creating the DBs
    In Rails 5 this can fail due to the new security setup surrounding
    the migrations. See #520

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -29,6 +29,8 @@ describe 'rails' do
           sh "bundle install"
           sh "rm -rf db/*.sqlite3"
           sh "bundle exec rake db:setup parallel:create 2>&1"
+          # Also test the case where the DBs need to be dropped
+          sh "bundle exec rake paralle:drop parallel:create 2>&1"
           sh "bundle exec rake parallel:prepare"
           sh "export RAILS_ENV=test && bundle exec rails runner User.create" # pollute the db
           out = sh "bundle exec rake parallel:prepare parallel:test"
