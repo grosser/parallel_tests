37d41d33b46f397a50103af8b0f5e105cc2475a4
Alexandre Wilhelm <contact.dogild@gmail.com>
2016-12-07 16:47:20 -0800

    Fixed: --group-by scenatios did not work anymore with the new version of Cucumber and Gherkin

--- lib/parallel_tests/cucumber/scenarios.rb
+++ lib/parallel_tests/cucumber/scenarios.rb
@@ -17,13 +17,71 @@ module ParallelTests
 
         private
 
-        def split_into_scenarios(files, tags=[])
-          tag_expression = ::Gherkin::TagExpression.new(tags)
-          scenario_line_logger = ParallelTests::Cucumber::Formatters::ScenarioLineLogger.new(tag_expression)
-          loader = ::Cucumber::Runtime::FeaturesLoader.new(files, [], tag_expression)
+        # Class stolen from cucumber as it was private
+        class NormalisedEncodingFile
+          COMMENT_OR_EMPTY_LINE_PATTERN = /^\s*#|^\s*$/ #:nodoc:
+          ENCODING_PATTERN = /^\s*#\s*encoding\s*:\s*([^\s]+)/ #:nodoc:
 
-          loader.features.each do |feature|
-            feature.accept(scenario_line_logger)
+          def self.read(path)
+            new(path).read
+          end
+
+          def initialize(path)
+            begin
+              @file = File.new(path)
+              set_encoding
+            rescue Errno::EACCES => e
+              raise FileNotFoundException.new(e, File.expand_path(path))
+            rescue Errno::ENOENT => e
+              raise FeatureFolderNotFoundException.new(e, path)
+            end
+          end
+
+          def read
+            @file.read.encode("UTF-8")
+          end
+
+          private
+
+          def set_encoding
+            @file.each do |line|
+              if ENCODING_PATTERN =~ line
+                @file.set_encoding $1
+                break
+              end
+              break unless COMMENT_OR_EMPTY_LINE_PATTERN =~ line
+            end
+            @file.rewind
+          end
+        end
+
+        def split_into_scenarios(files, tags=[])
+          tag_expression = ::Cucumber::Core::Gherkin::TagExpression.new(tags)
+          scenario_line_logger = ParallelTests::Cucumber::Formatters::ScenarioLineLogger.new(tag_expression)
+
+          features ||= files.map do |path|
+            source = NormalisedEncodingFile.read(path)
+            document = ::Cucumber::Core::Gherkin::Document.new(path, source)
+
+            parser  = ::Gherkin::Parser.new()
+            scanner = ::Gherkin::TokenScanner.new(document.body)
+            core_builder = ::Cucumber::Core::Gherkin::AstBuilder.new(document.uri)
+
+            begin
+              result = parser.parse(scanner)
+
+              result[:feature][:children].each do |feature_element|
+                if feature_element[:type] != 'Scenario'
+                  next
+                end
+
+                scenario_line_logger.visit_feature_element(document.uri, feature_element)
+              end
+
+            #receiver.feature core_builder.feature(result)
+            rescue *PARSER_ERRORS => e
+              raise Core::Gherkin::ParseError.new("#{document.uri}: #{e.message}")
+            end
           end
 
           scenario_line_logger.scenarios
