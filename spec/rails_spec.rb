39ad5974b82389a9b9b9e6d4cddb2e52d874c40c
Aleksei Gusev <aleksei.gusev@gmail.com>
2017-11-11 15:48:00 +0300

    File test for the gem installation against different versions of Rails

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -4,17 +4,20 @@ describe 'rails' do
   let(:test_timeout) { 240 } # this can take very long on fresh bundle ...
 
   def sh(command, options={})
-    result = `#{command}`
+    result = ''
+    IO.popen(options.fetch(:environment, {}), command) do |io|
+      result = io.read
+    end
     raise "FAILED #{command}\n#{result}" if $?.success? == !!options[:fail]
     result
   end
 
-  ["rails32", "rails42", "rails50"].each do |rails|
+  %w(rails32 rails42 rails50).each do |rails|
     it "can create and run #{rails}" do
       if RUBY_PLATFORM == "java"
         skip 'rails fixtures are not set up for java'
       elsif rails == 'rails32' && RUBY_VERSION >= '2.2.0'
-        skip 'rails 3.2 does not work on ruby >= 2.2.0'
+        skip 'rails 3.2 does not work  on ruby >= 2.2.0'
       elsif rails == 'rails50' && RUBY_VERSION < '2.3.0'
         skip 'rails 5.0 does not work on ruby < 2.3.0'
       end
@@ -28,11 +31,11 @@ describe 'rails' do
           sh "bundle config --local --path vendor/bundle"
           sh "bundle install"
           sh "rm -rf db/*.sqlite3"
-          sh "bundle exec rake db:setup parallel:create 2>&1"
+          sh "bundle exec rake db:setup parallel:create --trace 2>&1"
           # Also test the case where the DBs need to be dropped
           sh "bundle exec rake parallel:drop parallel:create"
           sh "bundle exec rake parallel:prepare"
-          sh "export RAILS_ENV=test && bundle exec rails runner User.create" # pollute the db
+          sh "bundle exec rails runner User.create", environment: {'RAILS_ENV' => 'test'} # pollute the db
           out = sh "bundle exec rake parallel:prepare parallel:test"
           expect(out).to match(/ 2 (tests|runs)/)
         end
