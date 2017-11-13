0486d6572538e084301cb27a2ef9fd0443795a01
Scott Olsen <solsen300@gmail.com>
2017-11-13 12:55:22 -0600

    skip test if ruby < 2.2

--- spec/parallel_tests/cucumber/failure_logger_spec.rb
+++ spec/parallel_tests/cucumber/failure_logger_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper'
 require 'parallel_tests/gherkin/io'
 require 'parallel_tests/cucumber/failures_logger'
 
-describe ParallelTests::Cucumber::FailuresLogger do
+describe ParallelTests::Cucumber::FailuresLogger, skip: RUBY_VERSION < "2.2" do
 
   before do
     @output = OutputLogger.new([])
     allow(@output).to receive(:write)
-    if RUBY_VERSION >= "2.2"
-      config = double('config', out_stream: @output, on_event: :test_run_finished)
-      @logger1 = ParallelTests::Cucumber::FailuresLogger.new(config)
-      @logger2 = ParallelTests::Cucumber::FailuresLogger.new(config)
-      @logger3 = ParallelTests::Cucumber::FailuresLogger.new(config)
-    else
-      @logger1 = ParallelTests::Cucumber::FailuresLogger.new(nil, @output, nil)
-      @logger2 = ParallelTests::Cucumber::FailuresLogger.new(nil, @output, nil)
-      @logger3 = ParallelTests::Cucumber::FailuresLogger.new(nil, @output, nil)
-    end
+
+    config = double('config', out_stream: @output, on_event: :test_run_finished)
+
+    @logger1 = ParallelTests::Cucumber::FailuresLogger.new(config)
+    @logger2 = ParallelTests::Cucumber::FailuresLogger.new(config)
+    @logger3 = ParallelTests::Cucumber::FailuresLogger.new(config)
 
     @feature1 = double('feature', :file => "feature/path/to/feature1.feature")
     @feature2 = double('feature', :file => "feature/path/to/feature2.feature")
