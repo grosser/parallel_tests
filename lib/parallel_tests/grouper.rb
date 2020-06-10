8ee28d9fd1bd738d98100b0ec8cb4807e459a988
David Rodríguez <deivid.rodriguez@riseup.net>
2020-06-10 17:30:43 +0200

    Move feature with steps grouping to a separate file
    For consistency.

--- lib/parallel_tests/grouper.rb
+++ lib/parallel_tests/grouper.rb
@@ -42,22 +42,8 @@ module ParallelTests
       end
 
       def group_by_features_with_steps(tests, options)
-        require 'cuke_modeler'
-
-        ignore_tag_pattern = options[:ignore_tag_pattern].nil? ? nil : Regexp.compile(options[:ignore_tag_pattern])
-        # format of hash will be FILENAME => NUM_STEPS
-        steps_per_file = tests.each_with_object({}) do |file,steps|
-          feature = ::CukeModeler::FeatureFile.new(file).feature
-
-          # skip feature if it matches tag regex
-          next if feature.tags.grep(ignore_tag_pattern).any?
-
-          # count the number of steps in the file
-          # will only include a feature if the regex does not match
-          all_steps = feature.scenarios.map{|a| a.steps.count if a.tags.grep(ignore_tag_pattern).empty? }.compact
-          steps[file] = all_steps.inject(0,:+)
-        end
-        steps_per_file.sort_by { |_, value| -value }
+        require 'parallel_tests/cucumber/features_with_steps'
+        ParallelTests::Cucumber::FeaturesWithSteps.all(tests, options)
       end
 
       def group_by_scenarios(tests, options={})
