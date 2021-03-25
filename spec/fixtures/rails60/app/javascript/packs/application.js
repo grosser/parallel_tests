b3e9c652ae9bdbff2834ba76b0cb08188fe58e10
David Rodríguez <deivid.rodriguez@riseup.net>
2021-03-25 17:27:50 +0100

    Remove activestorage to not pull mimemagic

--- spec/fixtures/rails60/app/javascript/packs/application.js
+++ spec/fixtures/rails60/app/javascript/packs/application.js
@@ -5,7 +5,6 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
 require("channels")
 
 
--- spec/fixtures/rails61/app/javascript/packs/application.js
+++ spec/fixtures/rails61/app/javascript/packs/application.js
@@ -5,9 +5,7 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 Turbolinks.start()
-ActiveStorage.start()
