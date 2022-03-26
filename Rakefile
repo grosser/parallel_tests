f23dec95695bd908c2564521de59f3e415d265c2
Michael Grosser <michael@grosser.it>
2022-03-26 12:54:20 -0700

    test on 3.1 (#843)
    * test on 3.1
    
    * run all rails folders we have
    
    * bundle update helper
    
    * rails 6 and ruby 3.1 do not mix

--- Rakefile
+++ Rakefile
@@ -13,3 +13,17 @@ desc "Run rubocop"
 task :rubocop do
   sh "rubocop --parallel"
 end
+
+desc "bundle all gemfiles [EXTRA=]"
+task :bundle_all do
+  extra = ENV["EXTRA"] || "install"
+
+  gemfiles = (["Gemfile"] + Dir["spec/fixtures/rails*/Gemfile"])
+  raise if gemfiles.size < 3
+
+  gemfiles.each do |gemfile|
+    Bundler.with_unbundled_env do
+      sh "GEMFILE=#{gemfile} bundle #{extra}"
+    end
+  end
+end
--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -13,7 +13,10 @@ describe 'rails' do
     result
   end
 
-  ['rails60', 'rails61', 'rails70'].each do |rails|
+  Dir["spec/fixtures/rails*"].each do |folder|
+    rails = File.basename(folder)
+    next if RUBY_VERSION >= "3.1.0" && rails < "rails70" # https://github.com/rails/rails/issues/43998
+
     it "can create and run #{rails}" do
       skip 'rails fixtures are not set up for java' if RUBY_PLATFORM == "java"
 
