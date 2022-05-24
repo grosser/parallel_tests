0c7609d412d64687d24e9f12eea0184aed7f82bc
daikimiura <daikiii5555@gmail.com>
2022-05-24 14:01:23 +0900

    Add spec for rails command with num_cpus

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -16,27 +16,58 @@ describe 'rails' do
     next if RUBY_VERSION >= "3.1.0" && rails < "rails70" # https://github.com/rails/rails/issues/43998
     next if RUBY_VERSION < "2.7.0" && rails >= "rails70"
 
-    it "can create and run #{rails}" do
-      skip 'rails fixtures are not set up for java' if RUBY_PLATFORM == "java"
+    context "without num_cpus" do
+      it "can create and run #{rails}" do
+        skip 'rails fixtures are not set up for java' if RUBY_PLATFORM == "java"
 
-      Dir.chdir("spec/fixtures/#{rails}") do
-        Bundler.with_unbundled_env do
-          # unset travis things
-          ENV.delete("RAILS_ENV")
-          ENV.delete("RACK_ENV")
+        Dir.chdir("spec/fixtures/#{rails}") do
+          Bundler.with_unbundled_env do
+            # unset travis things
+            ENV.delete("RAILS_ENV")
+            ENV.delete("RACK_ENV")
 
-          run ["bundle", "config", "--local", "path", "vendor/bundle"]
-          run ["bundle", "config", "--local", "frozen", "true"]
-          run ["bundle", "install"]
-          FileUtils.rm_f(Dir['db/*.sqlite3'])
-          run ["bundle", "exec", "rake", "db:setup", "parallel:create"]
-          # Also test the case where the DBs need to be dropped
-          run ["bundle", "exec", "rake", "parallel:drop", "parallel:create"]
-          run ["bundle", "exec", "rake", "parallel:setup"]
-          run ["bundle", "exec", "rake", "parallel:prepare"]
-          run ["bundle", "exec", "rails", "runner", "User.create"], environment: { 'RAILS_ENV' => 'test' } # pollute the db
-          out = run ["bundle", "exec", "rake", "parallel:prepare", "parallel:test"]
-          expect(out).to match(/ 2 (tests|runs)/)
+            run ["bundle", "config", "--local", "path", "vendor/bundle"]
+            run ["bundle", "config", "--local", "frozen", "true"]
+            run ["bundle", "install"]
+            FileUtils.rm_f(Dir['db/*.sqlite3'])
+            run ["bundle", "exec", "rake", "db:setup", "parallel:create"]
+            # Also test the case where the DBs need to be dropped
+            run ["bundle", "exec", "rake", "parallel:drop", "parallel:create"]
+            run ["bundle", "exec", "rake", "parallel:setup"]
+            run ["bundle", "exec", "rake", "parallel:prepare"]
+            run ["bundle", "exec", "rails", "runner", "User.create"], environment: { 'RAILS_ENV' => 'test' } # pollute the db
+            out = run ["bundle", "exec", "rake", "parallel:prepare", "parallel:test"]
+            expect(out).to match(/ 2 (tests|runs)/)
+          end
+        end
+      end
+    end
+
+    context "with num_cpus" do
+      it "can create and run #{rails}" do
+        skip 'rails fixtures are not set up for java' if RUBY_PLATFORM == "java"
+
+        num_cpus = 2
+
+        Dir.chdir("spec/fixtures/#{rails}") do
+          Bundler.with_unbundled_env do
+            # unset travis things
+            ENV.delete("RAILS_ENV")
+            ENV.delete("RACK_ENV")
+
+            run ["bundle", "config", "--local", "path", "vendor/bundle"]
+            run ["bundle", "config", "--local", "frozen", "true"]
+            run ["bundle", "install"]
+            FileUtils.rm_f(Dir['db/*.sqlite3'])
+            run ["bundle", "exec", "rake", "db:setup[#{num_cpus}]", "parallel:create[#{num_cpus}]"]
+            # Also test the case where the DBs need to be dropped
+            run ["bundle", "exec", "rake", "parallel:drop[#{num_cpus}]", "parallel:create[#{num_cpus}]"]
+            run ["bundle", "exec", "rake", "parallel:setup[#{num_cpus}]"]
+            run ["bundle", "exec", "rake", "parallel:prepare[#{num_cpus}]"]
+            run ["bundle", "exec", "rails", "runner", "User.create"], environment: { 'RAILS_ENV' => 'test' } # pollute the db
+            out = run ["bundle", "exec", "rake", "parallel:prepare[#{num_cpus}]", "parallel:test[#{num_cpus}]"]
+            expect(out).to match(/ 2 (tests|runs)/)
+          end
         end
       end
     end
