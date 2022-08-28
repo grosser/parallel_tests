ede5ea6450ee57c6b5be2ac75a2db3962a65ce16
Eric Kessler <morrow748@gmail.com>
2022-08-28 01:05:14 -0400

    Handle Cucumber Rules better
    Added a condition to some Rule logic so that older versions of
    CukeModeler that don't use Rules will still work.

--- lib/parallel_tests/cucumber/scenarios.rb
+++ lib/parallel_tests/cucumber/scenarios.rb
@@ -52,8 +52,8 @@ module ParallelTests
             feature_tags = feature.tags.map(&:name)
 
             # We loop on each children of the feature
-            # TODO: Handle versions of CukeModeler that don't have Rules
-            test_models = feature.tests + feature.rules.map(&:tests).flatten
+            test_models = feature.tests
+            test_models += feature.rules.map(&:tests).flatten if feature.respond_to?(:rules)
             test_models.each do |test|
               # It's a scenario, we add it to the scenario_line_logger
               scenario_line_logger.visit_feature_element(document.path, test, feature_tags, line_numbers: test_lines)
