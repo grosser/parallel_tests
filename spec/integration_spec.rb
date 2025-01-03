b8283774df6f2b32c31aad1cf6144e93fc391cfe
Michael Grosser <michael@grosser.it>
2025-01-03 09:13:33 -0800

    support running a tests in batches in a single process (#984)
    * support running a tests in batches in a single process
    
    * fix win

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -711,4 +711,27 @@ describe 'CLI' do
       expect(result).to_not include("Should not get here")
     end
   end
+
+  describe "--test-file-limit" do
+    let(:test_count) { 3 }
+    before do
+      test_count.times do |i|
+        write "spec/x#{i}_spec.rb", "puts %(TEST-\#{ENV['TEST_ENV_NUMBER']}-\#{Process.pid})"
+      end
+    end
+
+    it "runs in batches" do
+      result = run_tests ["spec"], type: 'rspec', add: ['--test-file-limit', '1', '--first-is-1', '-n', '2']
+      expect(result.scan(/TEST-\d/).sort).to eq(["TEST-1", "TEST-1", "TEST-2"])
+      pids = result.scan(/TEST-\d-(\d+)/).flatten.uniq
+      expect(pids.size).to eq test_count # did not run 2 tests in the same process
+    end
+
+    it "does not run in batches when above limit" do
+      result = run_tests ["spec"], type: 'rspec', add: ['--test-file-limit', '2', '--first-is-1', '-n', '2']
+      expect(result.scan(/TEST-\d/).sort).to eq(["TEST-1", "TEST-1", "TEST-2"])
+      pids = result.scan(/TEST-\d-(\d+)/).flatten.uniq
+      expect(pids.size).to eq 2
+    end
+  end
 end
